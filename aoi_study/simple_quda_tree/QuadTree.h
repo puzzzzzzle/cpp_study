4bd93c34637c8ded26af0bb88837d1ec16b71a1b
khalidzhang(张涛) <khalidzhang@tencent.com>
2019-12-09 16:07:44 +0800

    delete unuse file

--- aoi_study/simple_quda_tree/QuadTree.h
+++ aoi_study/simple_quda_tree/QuadTree.h
@@ -1,12 +1,3 @@
-/**
-	QuadTree.h
-	
-	QTNode: the recursive building block of a quad tree
-
-	Quadtree: a dynamic 2d space partitioning structure
-
-**/
-
 #ifndef QUADTREE_H
 #define QUADTREE_H
 
@@ -41,10 +32,9 @@ class QuadTree
 		void 	insert (vertex v, T data);
 		bool 	contains (vertex v);
 		bool 	remove (vertex v);
-//		void 	draw ();
 		void 	print (ostream &ss);
 		vector <pair <vertex, T> > getObjectsInRegion (vertex minXY, vertex maxXY);
-//		bool    clear();
+
 	private:
 
 		QTNode<T>* childNode (const vertex& v, QTNode<T>* node);
@@ -52,7 +42,6 @@ class QuadTree
 		int 	direction (const vertex& point, QTNode <T>* node);
 		void 	insert (vertex v, T data, QTNode<T>* node, unsigned depth);
 		void	reduce (stack <QTNode<T>*>& node);
-//		void 	draw (QTNode<T>* node);
 		void 	print (QTNode <T>* node, ostream& ss);
 		void	addAllPointsToResults (QTNode<T>* node, vector <pair <vertex, T> >& results);
 		bool	pointInRegion (const vertex& point, const vertex& minXY, const vertex& maxXY);
