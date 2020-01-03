e8028ecb58767faa474974286824bddebdef2713
khalidzhang <2359173906@qq.com>
2020-01-04 03:49:37 +0800

    add nasm linux support

--- nasm_study/c_call_nasm/main.cpp
+++ nasm_study/c_call_nasm/main.cpp
@@ -8,22 +8,23 @@
 extern "C" int64_t maxofthree(int64_t, int64_t, int64_t);
 
 TEST(test_test, 1) {
-    printf("%ld\n", maxofthree(1, -4, -7));
-    printf("%ld\n", maxofthree(2, -6, 1));
-    printf("%ld\n", maxofthree(2, 3, 1));
-    printf("%ld\n", maxofthree(-2, 4, 3));
-    printf("%ld\n", maxofthree(2, -6, 5));
-    printf("%ld\n", maxofthree(2, 4, 6));
+    EXPECT_EQ(maxofthree(1, -4, -7), 1);
+    EXPECT_EQ(maxofthree(2, -6, 1), 2);
+    EXPECT_EQ(maxofthree(2, 3, 1), 3);
+    EXPECT_EQ(maxofthree(-2, 4, 3), 4);
+    EXPECT_EQ(maxofthree(2, -6, 5), 5);
+    EXPECT_EQ(maxofthree(2, 4, 6), 6);
     EXPECT_EQ(1, 1);
 }
 
 int main(int argc, char **argv) {
     int iRet = 0;
     iRet = beforeRun();
-    if(iRet){
-        std::cerr<<"init fail with "<<iRet<<std::endl;
+    if (iRet) {
+        std::cerr << "init fail with " << iRet << std::endl;
     }
-    testing::InitGoogleTest(&argc,argv);
+    std::vector vector;
+    testing::InitGoogleTest(&argc, argv);
     iRet = RUN_ALL_TESTS();
     return iRet;
 }
--- nasm_study/nasm_pure/hello_world.asm
+++ nasm_study/nasm_pure/hello_world.asm
@@ -1,21 +1,29 @@
 ; ----------------------------------------------------------------------------------------
-; 仅使用系统调用来输出 "Hello, World" 到控制台。 这个程序仅在 64 位的 Linux 下运行。
+; 仅使用系统调用来输出 "Hello, World" 到控制台
 ; ----------------------------------------------------------------------------------------
 
-        global  _start
+section .data
 
-        section .text
+message:     db      "Hello, World1", 10      ; 注意到最后的换行
+messageLen:     equ     $-message       ;
+
+section .bss                            ; bss
+
+
+section .text
+
+global  _start
+
 _start:
         ; write(1, message, 13)
-        mov     rax, 1                  ; 1 号系统调用是写操作 
-        mov     rdi, 1                  ; 1 号文件系统调用是标准输出
-        mov     rsi, message            ; 输出字符串的地址 
-        mov     rdx, 13                 ; 字符串的长度 
-        syscall                         ; 调用系统执行写操作 
+        mov     eax, 4                  ; 1 号系统调用是写操作
+        mov     ebx, 1                  ; 1 号文件系统调用是标准输出
+        mov     ecx, message            ; 输出字符串的地址
+        mov     edx, messageLen                 ; 字符串的长度
+        int     80H                         ; 调用系统执行写操作
 
         ; exit(0)
-        mov     eax, 60                 ; 60 号系统调用是退出 
-        xor     rdi, rdi                ; 0 号系统调用作为退出 
-        syscall                         ; 调用系统执行退出 
-message:
-        db      "Hello, World", 10      ; 注意到最后的换行 
+        mov     eax, 1                 ; 60 号系统调用是退出
+        xor     ebx, ebx                ; 0 号系统调用作为退出
+        int     80H                         ; 调用系统执行退出
+
