16d5e25542892aaa7b094b01bf7a5822dfe929ae
khalidzhang <khalidzhang@tencent.com>
2020-08-11 19:31:37 +0800

    add some proto cmake support

--- common/CMake/FuncDefs.cmake
+++ common/CMake/FuncDefs.cmake
@@ -1,61 +1,23 @@
 # 获取目录的子目录
-#function(GetSubDirs Dir Result)
-#    file(GLOB Children ${Dir} ${Dir}/*)
-#    foreach(Child ${Children})
-#        if(NOT IS_DIRECTORY ${Child})
-#            continue()
-#        endif()
-#        if (${Child} STREQUAL ${Dir})
-#            continue()
-#        endif()
-#        list(APPEND AllDirs ${Child})
-#    endforeach()
-#    set(${Result} ${AllDirs} PARENT_SCOPE)
-#endfunction()
-
-# 获取目录的子目录
 function(GetSubDirs Dir Result)
+    message("get dirs in  ${Dir}")
+    #    EXECUTE_PROCESS(
+    #            COMMAND chmod +x ${CMAKE_SOURCE_DIR}/common/CMake/travel_dir.py
+    #    )
     EXECUTE_PROCESS(
-            COMMAND chmod +x ${CMAKE_SOURCE_DIR}/common/CMake/get_all_dirs.sh
-    )
-    EXECUTE_PROCESS(
-            COMMAND ${CMAKE_SOURCE_DIR}/common/CMake/get_all_dirs.sh ${Dir}
+            COMMAND python3 ${CMAKE_SOURCE_DIR}/common/CMake/travel_dir.py ${Dir} d
             OUTPUT_VARIABLE DIRS
     )
     set(${Result} ${DIRS} PARENT_SCOPE)
-    #    message("All dirs ${DIRS}")
+    message("All dirs ${DIRS}")
 endfunction()
 
-#set(auto_analisis_dir_list
-#        stl
-#        cpp_language
-#        boost_stud
-#        libgo_study
-#        libco_study
-#        aoi_study
-#        nasm_study
-#        )
-#foreach (auto_analisis_dir ${auto_analisis_dir_list})
-#    file(GLOB files RELATIVE ${PROJECT_SOURCE_DIR}/${auto_analisis_dir} CONFIGURE_DEPENDS ${PROJECT_SOURCE_DIR}/${auto_analisis_dir}/*)
-#    message("- ${auto_analisis_dir}")
-#    foreach (f ${files})
-#        set(build_temp ${auto_analisis_dir}_${f})
-#        #        message("\t ${PROJECT_SOURCE_DIR}/${auto_analisis_dir}/${f}/CMakeLists.txt")
-#        if (EXISTS ${PROJECT_SOURCE_DIR}/${auto_analisis_dir}/${f}/CMakeLists.txt)
-#            message("\t - ${build_temp}")
-#            add_subdirectory(${auto_analisis_dir}/${f})
-#        else ()
-#            message("\t - !!!${build_temp} without CMakeLists.txt, ignore!")
-#        endif ()
-#        #        message("\t\t - done")
-#    endforeach (f ${RPLACE_LIST})
-#endforeach ()
 
 function(AddAllSubDir)
     GetSubDirs(${CMAKE_SOURCE_DIR}/src SubDir)
     foreach (dir ${SubDir})
         #    message("sub : ${dir}")
-        string(REPLACE "${CMAKE_SOURCE_DIR}/src/" "" build_temp ${dir})
+        string(REPLACE "${CMAKE_SOURCE_DIR}/src/" "" build_temp ${dir} d)
         string(REGEX REPLACE "/" "_" build_temp ${build_temp})
         if (NOT EXISTS "${dir}/CMakeLists.txt")
             continue()
@@ -66,4 +28,14 @@ function(AddAllSubDir)
         message("- ${build_temp}")
         add_subdirectory(${dir})
     endforeach ()
+endfunction()
+
+function(BuildProto Dir OutPutDir)
+    message("build proto ")
+    EXECUTE_PROCESS(
+            COMMAND python3 ${CMAKE_SOURCE_DIR}/common/CMake/travel_dir.py ${Dir} build_proto ${OutPutDir}
+            OUTPUT_VARIABLE TEMP_OUT
+    )
+    message("All proto ${TEMP_OUT}")
+
 endfunction()
---
+++ src/cpp_language/protobuf_test/main.cpp
@@ -0,0 +1,36 @@
+//
+// Created by tao on 19-1-17.
+//
+#include "addressbook.pb.h"
+#include "common_includes.h"
+#include "cpp_reflact.hpp"
+#include "google/protobuf/util/json_util.h"
+using namespace std;
+namespace tutorial {
+REGISTER(Person);
+}
+
+TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+
+TEST(protobuf, 1) {
+  tutorial::Person test;
+  test.set_name("ahahahah");
+  test.set_id(222);
+  test.set_email("sefsgasdgfds");
+  LOG_DEBUG("sizeof Person" << sizeof(test));
+  // test.add_phones(tutorial::Person_PhoneNumber phone())
+  string buff;
+  google::protobuf::util::MessageToJsonString(test, &buff);
+  cout << buff;
+}
+int main(int argc, char **argv) {
+  int iRet = 0;
+  iRet = beforeRun();
+  if (iRet) {
+    std::cerr << "init fail with " << iRet << std::endl;
+  }
+  //    testing::InitGoogleTest();
+  testing::InitGoogleTest(&argc, argv);
+  iRet = RUN_ALL_TESTS();
+  return iRet;
+}
