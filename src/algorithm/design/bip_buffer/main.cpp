b188c9f5115e02a857e9558368b4a7cbe6e19789
khalidzhang <khalidzhang@tencent.com>
2020-09-11 12:22:18 +0800

    push to linux

--- src/algorithm/design/bip_buffer/main.cpp
+++ src/algorithm/design/bip_buffer/main.cpp
@@ -2,10 +2,13 @@
 // Created by tao on 19-1-17.
 //
 #include "common_includes.h"
+#include <string>
+#include <iostream>
+//TEST(test_test, 1) { EXPECT_EQ(1, 1); }
 
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
 
 int main(int argc, char **argv) {
+  std::string temp = "";
   int iRet = 0;
   iRet = beforeRun();
   if (iRet) {
