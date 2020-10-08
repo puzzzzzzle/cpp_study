c1dfbecd934abdff9dd1a715e35537cfbbe1a726
khalidzhang <khalidzhang@tencent.com>
2020-10-08 22:03:36 +0800

    rebuild project

--- formate_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-PROCESS_DIRS="common src shell"
-PROCESS_CMD="clang-format -style=file -i"
-
-find ${PROCESS_DIRS} -regextype posix-extended -regex ".*\.(h|hpp|cpp|cc|c|proto)" |xargs -I{} -t ${PROCESS_CMD} {}
