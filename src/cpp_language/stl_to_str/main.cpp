31e5dd2f54a453fd8b2c627103d1ff415948975f
puzzzzzzle <2359173906@qq.com>
2022-04-02 14:00:42 +0800

    change value name, support tuple

--- src/cpp_language/stl_to_str/main.cpp
+++ src/cpp_language/stl_to_str/main.cpp
@@ -32,22 +32,32 @@ std::ostream& operator<<(std::ostream& os, const TestPayload& p) {
   return os;
 }
 struct TestPayloadHash {
-  size_t operator()(TestPayload t) const { return static_cast<size_t>(t.v1); }
+  size_t operator()(const TestPayload& t) const {
+    return static_cast<size_t>(t.v1);
+  }
 };
 class TestPayloadValueToStream {
   public:
-  void operator()(std::ostream& os, const TestPayload& v) {
+  static void to_stream(std::ostream& os, const TestPayload& v) {
     os << v << "@" << &v;
   }
 };
 
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
-
 TEST(vec, int) {
   std::vector<int> v1{1, 2, 3, 4};
-  vec_like_to_string(v1);
   LOG_DEBUG(v1)
   LOG_DEBUG(vec_like_to_string(v1))
+}
+
+TEST(vec, recursive) {
+  std::vector<int> v1{};
+  std::vector<std::vector<int>> vv1{};
+  for (int i = 0; i < 10; ++i) {
+    v1.push_back(i);
+    vv1.push_back(v1);
+  }
+  LOG_DEBUG(v1)
+  LOG_DEBUG(vv1)
 }
 TEST(array, TestPayload) {
   std::array<TestPayload, 5> v1{};
@@ -141,10 +151,21 @@ TEST(map, TestPayload) {
   LOG_DEBUG((map_like_to_string<decltype(v1), default_value_to_stream<int>,
                                 TestPayloadValueToStream>(v1)))
 }
+TEST(map, recursive) {
+  std::map<int, TestPayload> v1{};
+  std::map<int, std::map<int, TestPayload>> vv1{};
+
+  for (int i = 0; i < 5; ++i) {
+    v1[i] = TestPayload::Get();
+    vv1[i*10000] = v1;
+  }
+  LOG_DEBUG(v1)
+  LOG_DEBUG(vv1)
+}
 TEST(multimap, TestPayload) {
   std::multimap<int, TestPayload> v1{};
   for (int i = 0; i < 5; ++i) {
-    v1.insert({i,TestPayload::Get()});
+    v1.insert({i, TestPayload::Get()});
   }
   LOG_DEBUG(v1)
   LOG_DEBUG(map_like_to_string(v1))
@@ -154,9 +175,9 @@ TEST(multimap, TestPayload) {
 TEST(unordered_multimap, TestPayload) {
   std::unordered_multimap<int, TestPayload> v1{};
   for (int i = 0; i < 5; ++i) {
-    v1.insert({i,TestPayload::Get()});
+    v1.insert({i, TestPayload::Get()});
   }
-//  LOG_DEBUG(v1)
+  //  LOG_DEBUG(v1)
   LOG_DEBUG(map_like_to_string(v1))
   LOG_DEBUG((map_like_to_string<decltype(v1), default_value_to_stream<int>,
                                 TestPayloadValueToStream>(v1)))
@@ -175,6 +196,13 @@ TEST(pair, TestPayload) {
   auto v1 = std::make_pair(1, TestPayload::Get());
   LOG_DEBUG(v1)
   LOG_DEBUG(pair_to_string(v1))
+}
+TEST(tuple, TestPayload) {
+  LOG_DEBUG(std::make_tuple(1))
+  LOG_DEBUG(std::make_tuple(1, TestPayload::Get()))
+  LOG_DEBUG(std::make_tuple(1, TestPayload::Get(), TestPayload::Get()))
+  LOG_DEBUG(std::make_tuple(1, TestPayload::Get(),
+                            std::make_tuple(1, TestPayload::Get())))
 }
 int main(int argc, char** argv) {
   int iRet = 0;
