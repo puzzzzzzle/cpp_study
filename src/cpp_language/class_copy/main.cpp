0ba7a89c172ed659b81efdc2dff4513c97e84fab
puzzzzzzle <2359173906@qq.com>
2022-04-27 22:13:21 +0800

    add

--- src/cpp_language/class_copy/main.cpp
+++ src/cpp_language/class_copy/main.cpp
@@ -18,7 +18,45 @@ TEST(oper, 2) {
   auto obj = get_big_obj_1(42);
 }
 
+class Base{};
+std::ostream& operator<<(std::ostream& os, const Base & obj)
+{
+  os << "cref \n";
+  return os;
+}
+//std::ostream& operator<<(std::ostream& os, const std::shared_ptr<Base> & obj)
+//{
+//  os << "sptr \n";
+//  return os;
+//}
+//std::ostream& operator<<(std::ostream& os, const std::shared_ptr<const Base> & obj)
+//{
+//  os << "csptr \n";
+//  return os;
+//}
+//std::ostream& operator<<(std::ostream& os, const Base * obj)
+//{
+//  os << "ptr \n";
+//  return os;
+//}
+std::ostream& operator<<(std::ostream& os, const Base * const obj)
+{
+  os << "cptr \n";
+  return os;
+}
+class Sub : public Base{};
 
+TEST(oper, 3) {
+  auto obj = std::make_shared<Sub>();
+  std::cout << *obj;
+  std::cout << obj;
+  std::cout << obj.get();
+  const std::shared_ptr<const Base> cobj = obj;
+  std::cout << *cobj;
+  std::cout << cobj;
+  std::cout << cobj.get();
+
+}
 int main(int argc, char **argv) {
   int iRet = 0;
 
---
+++ src/cpp_language/has_global_func/main.cpp
@@ -0,0 +1,31 @@
+//
+// Created by tao on 19-1-17.
+//
+#include "common_includes.h"
+#include "has_global_func.h"
+TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+
+// 这一刻, 是不是存在 foo 函数, 之后再申明不管
+HAS_GLOBAL_FUNC(foo)
+// foo 没有声明
+static_assert(!has_foo::check::value);
+// 申明foo
+void foo();
+// 在 HAS_GLOBAL_FUNC(foo) 已经绑定了, foo还是不存在
+static_assert(!has_foo::check::value);
+
+// 声明一个func
+void func();
+HAS_GLOBAL_FUNC(func)
+static_assert(has_func::check::value);
+
+int main(int argc, char **argv) {
+  int iRet = 0;
+  iRet = beforeRun();
+  if (iRet) {
+    std::cerr << "init fail with " << iRet << std::endl;
+  }
+  testing::InitGoogleTest(&argc, argv);
+  iRet = RUN_ALL_TESTS();
+  return iRet;
+}
