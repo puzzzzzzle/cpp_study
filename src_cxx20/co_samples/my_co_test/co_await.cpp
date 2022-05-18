716dc02d440f819c919103189de6fb621980f415
puzzzzzzle <2359173906@qq.com>
2022-05-18 21:25:24 +0800

    add test cod

--- src_cxx20/co_samples/my_co_test/co_await.cpp
+++ src_cxx20/co_samples/my_co_test/co_await.cpp
@@ -11,46 +11,65 @@
 #include "log_init.h"
 
 namespace co_await_test {
-struct task {
-  struct promise_type {
-    task get_return_object() { return {}; }
-    std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() noexcept { return {}; }
-    void return_void() {}
-    void unhandled_exception() {}
-  };
+struct awaitable {
+  int64_t ms_{};
+  std::thread t_{};
+  bool await_ready() {
+    LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+
+    return false;
+  }
+  void await_suspend(std::coroutine_handle<> h) {
+    LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+  }
+  void await_resume() { LOG_DEBUG("call : " << __PRETTY_FUNCTION__) }
 };
-auto thread_wait_timer(int64_t ms) {
-  struct thread_awaitable {
-    int64_t ms_{};
-    std::thread t_{};
-    bool await_ready() {
-      LOG_DEBUG("call")
-      return false;
+auto awaitable_test() { return awaitable{}; }
+struct task {
+  struct promise_type;
+  using handle_type = std::coroutine_handle<promise_type>;
+
+  struct promise_type {
+    task get_return_object() {
+      LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+      return handle_type::from_promise(*this);
     }
-    void await_suspend(std::coroutine_handle<> h) {
-      LOG_DEBUG("start")
-      int64_t sleepMs = ms_;
-      t_ = std::thread([h, sleepMs]() {
-        LOG_DEBUG("wait")
-        std::this_thread::sleep_for(std::chrono::milliseconds(sleepMs));
-        LOG_DEBUG("resume")
-        h.resume();
-      });
-      LOG_DEBUG("end")
+    std::suspend_never initial_suspend() {
+      LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+      return {};
     }
-    void await_resume() { LOG_DEBUG("call") }
+    std::suspend_never final_suspend() noexcept {
+      LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+      return {};
+    }
+    void destroy() const noexcept {
+      LOG_DEBUG("call : " << __PRETTY_FUNCTION__)
+    }
+
+    void return_void() { LOG_DEBUG("call : " << __PRETTY_FUNCTION__) }
+    void unhandled_exception() { LOG_DEBUG("call : " << __PRETTY_FUNCTION__) }
   };
-  return thread_awaitable{ms};
-}
-task resume_by_thread_timer() {
+  task(const handle_type& h) : h_(h) {}
+  handle_type h_{};
+};
+auto resume_by_thread_timer() -> task {
   LOG_DEBUG("start")
-  auto awaitable = thread_wait_timer(1000);
+  // 生成 等待体, 但是不执行
+  auto awaitable = awaitable_test();
+  // 执行 完 await_ready await_suspend 后 这个函数直接返回一个 task
+  // 对象到调用者上
   co_await awaitable;
 
-  // TODO co_await 后 就直接保存状态返回调用方了, 到不了这里
+  // 这个并不会被立即执行 需要等 协程 resume 后才会
   LOG_DEBUG("end")
-  awaitable.t_.join();
 }
-TEST(co_await_test, 1) { resume_by_thread_timer(); }
+TEST(co_await_test, 1) {
+  LOG_DEBUG("start call coroutine")
+  auto t = resume_by_thread_timer();
+  LOG_DEBUG("start resume coroutine")
+  t.h_.resume();
+  LOG_DEBUG("end resume coroutine")
+  // 不用destroy
+  //  t.h_.destroy();
+}
 }  // namespace co_await_test
