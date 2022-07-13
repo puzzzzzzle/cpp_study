13687482a61a9c9d423725e01b079284bae473aa
puzzzzzzle <2359173906@qq.com>
2022-07-13 23:54:12 +0800

    add

---
+++ src/linux_funcs/dl_funcs/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by tao on 19-1-17.
+//
+#include "common_includes.h"
+#include <dlfcn.h>
+
+
+TEST(test_test, 1) {
+  auto *dl = dlopen("test.so",RTLD_LAZY);
+
+
+}
+
+int main(int argc, char **argv) {
+  int iRet = 0;
+  iRet = beforeRun();
+  if (iRet) {
+    std::cerr << "init fail with " << iRet << std::endl;
+  }
+  testing::InitGoogleTest(&argc, argv);
+  iRet = RUN_ALL_TESTS();
+  return iRet;
+}
