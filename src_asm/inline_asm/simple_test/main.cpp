e020d85f64d114f1a3e12cc1e443cd65eee2a7b3
puzzzzzzle <2359173906@qq.com>
2022-08-20 19:32:24 +0800

    add inline asm tests

---
+++ src_asm/inline_asm/simple_test/main.cpp
@@ -0,0 +1,28 @@
+//
+// Created by tao on 19-1-17.
+//
+#include "common_includes.h"
+
+void inline_asm_test() {
+  int64_t a{10}, b{};
+  asm(R"(movq %1,%%rax;
+movq %%rax,%0
+)"
+      : "=r"(b)
+      : "r"(a)
+      : "%rax");
+  LOG_DEBUG("a:" << a << "\tb:" << b)
+}
+
+TEST(simple, asm_1) { inline_asm_test(); }
+
+int main(int argc, char **argv) {
+  int iRet = 0;
+  iRet = beforeRun();
+  if (iRet) {
+    std::cerr << "init fail with " << iRet << std::endl;
+  }
+  testing::InitGoogleTest(&argc, argv);
+  iRet = RUN_ALL_TESTS();
+  return iRet;
+}
