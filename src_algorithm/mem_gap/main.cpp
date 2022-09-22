535474fde72f32471441d559eb59da215138126e
puzzzzzzle <2359173906@qq.com>
2022-09-22 15:36:23 +0800

    add mem gap

--- src_algorithm/mem_gap/main.cpp
+++ src_algorithm/mem_gap/main.cpp
@@ -3,18 +3,84 @@
 //
 #include "common_includes.h"
 #include "mem_gap.h"
-
-TEST(mem, 1) {
-  char *t = new char[1024 * 1024];
-  t[sizeof(t) - 5] = 1;
-  MemGap gap{};
-  malloc_stats();
+TEST(get_pid, 1) {
   LOG_DEBUG("pid:" << getpid() << "\ttid:" << gettid() << "\tgid:" << getgid())
   auto r = std::thread([]() {
     LOG_DEBUG("pid:" << getpid() << "\ttid:" << gettid()
                      << "\tgid:" << getgid())
   });
   r.join();
+}
+TEST(mem, Run) {
+  std::vector<char *> tmp{};
+  for (int i = 0; i < 10000; ++i) {
+    char *t = new char[1024];
+    t[sizeof(t) - 1] = 1;
+    tmp.push_back(t);
+  }
+  LOG_DEBUG("statm rss " << GetStatm().m_resident)
+  LOG_DEBUG("getrusage rss " << GetUsage().ru_maxrss)
+  LOG_DEBUG("malloc_info " << GetMallocInfo())
+  malloc_stats();
+  for (auto item : tmp) {
+    delete[] item;
+  }
+}
+
+TEST(mem, Currect) {
+  MemGap gap{};
+  LOG_DEBUG("mem gap " << gap.GapRssAndReset() << "\tnow\t" << gap.NowRss())
+  std::atomic_int p{};
+  int buffSize = 102400;
+  char **buff = new char *[buffSize];
+  LOG_DEBUG("mem gap " << gap.GapRssAndReset() << "\tnow\t" << gap.NowRss())
+  auto allocSize = [&](size_t size, int count,
+                       char initValue) -> std::vector<int> {
+    std::vector<int> result{};
+    for (int i = 0; i < count; ++i) {
+      auto currP = p++;
+      assert(currP < buffSize);
+      char *tmp = new char[size];
+      buff[currP] = tmp;
+      if (initValue != 0) {
+        memset(tmp, initValue, size);
+      }
+      result.push_back(currP);
+    }
+    return result;
+  };
+  auto deletePointer = [&](std::vector<int> &ps) {
+    for (auto point : ps) {
+      assert(point < buffSize);
+      delete[] buff[point];
+      buff[point] = nullptr;
+    }
+  };
+  // 10k * 10240
+  auto allocPointer = allocSize(10 * 1024, 10240, 1);
+  LOG_DEBUG("create : mem gap " << gap.GapRssAndReset() << "\tnow\t"
+                                << gap.CurrRecord())
+
+  // delete
+  // not free now
+  deletePointer(allocPointer);
+  LOG_DEBUG("delete but not malloc_trim :mem gap " << gap.GapRssAndReset() << "\tnow\t"
+                                            << gap.CurrRecord())
+  malloc_trim(0);
+  LOG_DEBUG("malloc_trim : mem gap " << gap.GapRssAndReset() << "\tnow\t"
+                                  << gap.CurrRecord())
+
+  auto r = GetUsage();
+  {
+    auto currP = p.load();
+    ASSERT_TRUE(currP < buffSize);
+    for (int i = 0; i < currP; ++i) {
+      delete[] buff[i];
+    }
+    delete[] buff;
+  }
+  LOG_DEBUG("exit : mem gap " << gap.GapRssAndReset() << "\tnow\t"
+                              << gap.NowRss())
 }
 int main(int argc, char **argv) {
   int iRet = 0;
--- src_algorithm/mem_gap/mem_gap.h
+++ src_algorithm/mem_gap/mem_gap.h
@@ -11,56 +11,120 @@
 
 #include <fcntl.h>
 #include <malloc.h>
+#include <sys/resource.h>
 
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <unistd.h>
 
-struct CurrProcessMemInfo {
-  size_t m_inUse{};
+// 获取 top 中展示的内存格式数据
+struct Statm {
+  // 单位: kB
+  // VIRT, RES,SHR
+  unsigned long m_size, m_resident, m_share, m_text, m_lib, m_data, m_dt;
 };
+/**
+ *  获取 top 中展示的内存格式数据
+ *  单位 pageSize
+ * @return
+ */
+inline Statm GetStatm() {
+  //  auto pagesize = getpagesize();
+  Statm result{};
+  const char* statm_path = "/proc/self/statm";
 
+  FILE* f = fopen(statm_path, "r");
+  if (!f) {
+    perror(statm_path);
+    fclose(f);
+    return {};
+  }
+  if (7 != fscanf(f, "%ld %ld %ld %ld %ld %ld %ld", &result.m_size,
+                  &result.m_resident, &result.m_share, &result.m_text,
+                  &result.m_lib, &result.m_data, &result.m_dt)) {
+    perror(statm_path);
+    fclose(f);
+    return {};
+  }
+  // convert to byte
+  //  decltype(result.m_size)* tmp = &result.m_size;
+  //  for (int i = 0; i < sizeof(result) / sizeof(result.m_size); ++i) {
+  //    tmp[i] *= (pagesize / 1024);
+  //  }
+  fclose(f);
+  return result;
+}
+
+/**
+ * 获取malloc info 中统计的内存信息
+ * 精确到byte, 但是需要自己手动接xml, 还没处理
+ * 单位: byte
+ * @return xml 格式
+ */
+inline std::string GetMallocInfo() {
+  // crete buff
+  std::string result{};
+  char* buffer = NULL;
+  size_t bufferSize = 0;
+  auto ss = open_memstream(&buffer, &bufferSize);
+
+  // get status
+  int ret = malloc_info(0, ss);
+  fflush(ss);
+  if (ret) {
+    fprintf(stderr, "call malloc_info fail %d", ret);
+    result = "error";
+  } else {
+    result = std::string(buffer);
+  }
+
+  // close
+  fclose(ss);
+  free(buffer);
+  return result;
+}
+/**
+ * 获取 getrusage 信息, 应该是VIRT
+ * 单位: kB
+ * @return
+ */
+inline rusage GetUsage() {
+  rusage u{};
+  getrusage(RUSAGE_SELF, &u);
+  return u;
+}
+inline void GetUsage(rusage& u) { getrusage(RUSAGE_SELF, &u); }
+/**
+ * 内存统计GAP
+ * 单位 kB
+ */
 class MemGap {
   private:
-  CurrProcessMemInfo m_info{};
+  long m_rss{};
 
   public:
-  MemGap() { m_info = GetCurrProcessMem(); }
-  CurrProcessMemInfo GetCurrProcessMem() {
-    // crete buff
-    char* buffer = NULL;
-    size_t bufferSize = 0;
-    auto ss = open_memstream(&buffer, &bufferSize);
+  void ResetNow() { m_rss = NowRss(); }
+  long CurrRecord() { return m_rss; }
+  static long NowRss() {
+    return (long)GetStatm().m_resident * getpagesize() / 1024;
+  }
 
-    // get status
-    int ret = malloc_info(0, ss);
-    fflush(ss);
-    if (ret) {
-      fprintf(stderr, "call malloc_info fail %d", ret);
-      return {};
-    }
-
-    // parse memInfo
-    printf("%s\n", buffer);
-    CurrProcessMemInfo info{};
-
-    // close
-    fclose(ss);
-    free(buffer);
-
-    {
-      auto procName =
-          std::string("/proc/" + std::to_string(getpid()) + "/status");
-      std::fstream procFile(procName, std::ios::in);
-      std::ostringstream oss{};
-      while (procFile.good() && !procFile.eof())
-      {
-        std::string temp{};
-        procFile>>temp;
-        oss<<temp<<"\n";
-      }
-      printf("%s\n", oss.str().c_str());
-    }
-    return info;
+  public:
+  MemGap() { ResetNow(); }
+  /**
+   * 当前内存和上次存档的变化
+   * @return
+   */
+  long GapRss() { return NowRss() - m_rss; }
+  /**
+   * 当前内存和上次存档的变化, 并重置记录为当前内存大小
+   * @return
+   */
+  long GapRssAndReset() {
+    auto curr = NowRss();
+    auto ret = curr - m_rss;
+    m_rss = curr;
+    return ret;
   }
 };
