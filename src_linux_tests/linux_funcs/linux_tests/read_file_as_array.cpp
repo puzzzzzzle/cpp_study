b8e08fb5f6d6e56d4073020e4ac1d8de1bc6a720
puzzzzzzle <2359173906@qq.com>
2023-02-07 09:52:28 +0800

    update log

--- src_linux_tests/linux_funcs/linux_tests/read_file_as_array.cpp
+++ src_linux_tests/linux_funcs/linux_tests/read_file_as_array.cpp
@@ -2,8 +2,22 @@
 // Created by khalidzhang on 2023/1/21.
 //
 #include "common_includes.h"
+#include <fstream>
 TEST(read,1)
 {
   LOG_DEBUG("read file ")
-
+  std::ifstream in("./data/boost_log_settings.ini",std::ios::binary|std::ios::in);
+  ASSERT_TRUE(in.good());
+  char buff[1024];
+  size_t len{};
+  std::ostringstream oss{};
+  while (len = in.readsome(buff,sizeof (buff)))
+  {
+    for(size_t i = 0;i<len;++i)
+    {
+      isprint()
+      oss<<buff[i];
+    }
+  }
+  LOG_DEBUG(oss.str())
 }
