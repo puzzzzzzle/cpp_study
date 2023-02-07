66fcef4b19392e2e85fb183e7324dc69044e8479
puzzzzzzle <2359173906@qq.com>
2023-02-07 11:09:41 +0800

    add kcp

---
+++ src_lib_tests/kcp/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by tao on 19-1-17.
+//
+#include "common_includes.h"
+TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+
+int main(int argc, char **argv) {
+  int iRet = 0;
+  testing::InitGoogleTest(&argc, argv);
+  iRet = RUN_ALL_TESTS();
+  sleep(1);
+  return iRet;
+}
