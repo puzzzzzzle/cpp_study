4de67d8d12ac354ca084b272b3eb9bceed80684c
puzzzzzzle <2359173906@qq.com>
2023-02-22 15:36:08 +0800

    update msg def

--- src_lib_tests/boost_asio/msg_def.h
+++ src_lib_tests/boost_asio/msg_def.h
@@ -14,6 +14,27 @@
 #include <cstring>
 #include <string>
 namespace MsgHead {
+#define bswap_16(value) ((((value)&0xff) << 8) | ((value) >> 8))
+
+#define bswap_32(value)                                     \
+  (((uint32_t)bswap_16((uint16_t)((value)&0xffff)) << 16) | \
+   (uint32_t)bswap_16((uint16_t)((value) >> 16)))
+
+inline void WriteUint32(char* pos, uint32_t val) {
+  if constexpr (std::endian::native != std::endian::little) {
+    val = bswap_32(val);
+  }
+  memcpy(pos, &val, sizeof(uint32_t));
+}
+inline uint32_t ReadUint32(char* pos) {
+  uint32_t val;
+  memcpy(&val, pos, sizeof(uint32_t));
+  if constexpr (std::endian::native != std::endian::little) {
+    val = bswap_32(val);
+  }
+  return val;
+}
+
 enum class MsgType : uint8_t {
   kReq = 0,
   kRsp,
@@ -22,11 +43,11 @@ enum class MsgType : uint8_t {
 enum class Flags {
   // 分包标记， 读到 如果被分包， 前面的都有这个标记， 最后一个没有
   kSplit = 0x1,
-
+  // 自定义包头是否存在， 如果被分包， 前面的包优先填充自定义包头，
+  // 完成后后面才是包体
+  kCustomHead = 0x2,
   // 最多8个标记 0x80
 };
-#define BIG_LITTLE_ENDIAN_SWAP_16(A) \
-  ((((uint16_t)(A)&0xff00) >> 8) | (((uint16_t)(A)&0x00ff) << 8))
 struct MsgHead {
   uint16_t seq;      // 2b
   MsgType msg_type;  // 1b
@@ -38,97 +59,127 @@ struct MsgHead {
     // x86 是小端序, arm虽然可配置, 但是一般也是小端序
     // 这里使用小端序来存储
     if constexpr (std::endian::native != std::endian::little) {
-      uint16_t s = BIG_LITTLE_ENDIAN_SWAP_16(seq);
+      uint16_t s = bswap_16(seq);
       memcpy(dest, &s, sizeof(seq));
     }
   }
   void FromBuf(const char* from) {
     memcpy(this, from, sizeof(MsgHead));
     if constexpr (std::endian::native != std::endian::little) {
-      seq = BIG_LITTLE_ENDIAN_SWAP_16(seq);
+      seq = bswap_16(seq);
     }
   }
-#undef BIG_LITTLE_ENDIAN_SWAP_16
 };
 static_assert(sizeof(MsgHead) == 4);
-template <ssize_t PadSize>
 class MsgBufBase {
-  public:
-  static constexpr ssize_t PadSize_ = PadSize;
-  static constexpr ssize_t ExternSize_ = PadSize_ + sizeof(MsgHead);
-
   protected:
   char* buf_;
-  ssize_t size_{};
-
-  void Init(ssize_t size) {
-    size_ = ExternSize_ + size;
+  ssize_t size_;
+  // custom_head_size_ 已经写在buff中了， 这里冗余了一份， 用来加速
+  uint32_t custom_head_size_;
+  static constexpr ssize_t kCustomHeadLen = 4;
+  void Init(ssize_t size, ssize_t custom_head_size) {
+    // 自定义包头长度
+    size_ = sizeof(MsgHead) + size;
+    custom_head_size_ = custom_head_size;
+    if (custom_head_size > 0) {
+      size_ += (custom_head_size + kCustomHeadLen);
+    }
     buf_ = new char[size_];
+    MsgHead head{};
+    if (custom_head_size > 0) {
+      head.flags |= (uint8_t)Flags::kCustomHead;
+      WriteUint32(buf_ + sizeof(MsgHead), custom_head_size);
+    }
+    SetHead(head);
   }
 
   public:
-  MsgBufBase(ssize_t size) { Init(size); }
-  MsgBufBase(const char* buf, ssize_t size) {
-    Init(size);
-    memcpy(buf_ + ExternSize_, buf, size);
+  void SetHead(const MsgHead& head) { head.ToBuf(buf_); }
+  void ReadHead(MsgHead& head) const { head.FromBuf(buf_); }
+  char* Buf() { return buf_ + ExternSize(); }
+  ssize_t BufSize() const { return size_ - ExternSize(); }
+
+  bool HadCustomHead() const { return custom_head_size_ > 0; }
+  char* CustomHead() {
+    if (!HadCustomHead()) {
+      return nullptr;
+    }
+    return buf_ + sizeof(MsgHead) + kCustomHeadLen;
   }
-  MsgBufBase(const std::string& buf) {
-    Init(buf.size());
-    memcpy(buf_ + ExternSize_, buf.data(), buf.size());
+  ssize_t CustomHeadSize() const { return custom_head_size_; }
+  ssize_t ExternSize() const {
+    if (HadCustomHead()) {
+      return sizeof(MsgHead) + CustomHeadSize() + kCustomHeadLen;
+    }
+    return sizeof(MsgHead);
+  }
+
+  public:
+  MsgBufBase(ssize_t size, ssize_t custom_head_size) {
+    Init(size, custom_head_size);
+  }
+  MsgBufBase(const char* buf, ssize_t size, ssize_t custom_head_size) {
+    Init(size, custom_head_size);
+    memcpy(buf_ + ExternSize(), buf, size);
+  }
+  MsgBufBase(const std::string& buf, ssize_t custom_head_size) {
+    Init(buf.size(), custom_head_size);
+    memcpy(buf_ + ExternSize(), buf.data(), buf.size());
   }
   ~MsgBufBase() { delete[] buf_; }
 
   void CopyFrom(const MsgBufBase& buf) {
     size_ = buf.size_;
     buf_ = new char[size_];
+    custom_head_size_ = buf.custom_head_size_;
     memcpy(buf_, buf.buf_, size_);
   }
   MsgBufBase(const MsgBufBase& buf) { CopyFrom(buf); }
-  MsgBufBase& operator=(const MsgBufBase& buf) { CopyFrom(buf); }
+  MsgBufBase& operator=(const MsgBufBase& buf) {
+    CopyFrom(buf);
+    return *this;
+  }
 
   void MoveFrom(MsgBufBase& buf) noexcept {
     size_ = buf.size_;
     buf_ = buf.buf_;
+    custom_head_size_ = buf.custom_head_size_;
     buf.size_ = 0;
     buf.buf_ = nullptr;
+    buf.custom_head_size_ = 0;
   }
   MsgBufBase(MsgBufBase&& buf) noexcept { MoveFrom(buf); }
-  MsgBufBase& operator=(MsgBufBase&& buf) noexcept { MoveFrom(buf); }
-
-  void SetHead(const MsgHead& head) { head.ToBuf(buf_); }
-  void ReadHead(MsgHead& head) const { head.FromBuf(buf_); }
-
-  char* Buf() { return buf_ + ExternSize_; }
-  ssize_t BufSize() const { return size_ - ExternSize_; }
-};
-template <ssize_t PadSize>
-class PadMsgBuf : public MsgBufBase<PadSize> {
-  public:
-  using BufBaseT = MsgBufBase<PadSize>;
-  using MsgBufBase<PadSize>::MsgBufBase;
-  char* PadBuf() {
-    if constexpr (PadSize == 0) {
-      return nullptr;
-    }
-    return BufBaseT::buf_ + sizeof(MsgHead);
+  MsgBufBase& operator=(MsgBufBase&& buf) noexcept {
+    MoveFrom(buf);
+    return *this;
   }
-  ssize_t PadBufSize() const { return PadSize; }
 };
+class CustomHeadMsgBuf : public MsgBufBase {
+  public:
+  using BufBaseT = MsgBufBase;
+  using MsgBufBase::MsgBufBase;
+};
+
 template <class T>
-class PadStructMsgBuf : public MsgBufBase<sizeof(T)> {
+class CustomStructHeadMsgBuf : public MsgBufBase {
   public:
-  using BufBaseT = MsgBufBase<sizeof(T)>;
-  using MsgBufBase<sizeof(T)>::MsgBufBase;
-  void SetPad(const T& pad) {
-    pad.ToBuf(BufBaseT::buf_ + sizeof(MsgHead), BufBaseT::PadSize_);
+  using BufBaseT = MsgBufBase;
+  using MsgBufBase::MsgBufBase;
+  CustomStructHeadMsgBuf(ssize_t size) : MsgBufBase(size, sizeof(T)) {}
+  CustomStructHeadMsgBuf(const char* buf, ssize_t size)
+      : MsgBufBase(buf, size, sizeof(T)) {}
+  CustomStructHeadMsgBuf(const std::string& buf) : MsgBufBase(buf, sizeof(T)) {}
+  void SetCustomHead(const T& head) {
+    head.ToBuf(CustomHead(), CustomHeadSize());
   }
-  void ReadPad(T& pad) {
-    pad.FromBuf(BufBaseT::buf_ + sizeof(MsgHead), BufBaseT::PadSize_);
-  }
+  void ReadCustomHead(T& head) { head.FromBuf(CustomHead(), CustomHeadSize()); }
 };
-class MsgBuf : public MsgBufBase<0> {
+class MsgBuf : public MsgBufBase {
   public:
-  using BufBaseT = MsgBufBase<0>;
-  using BufBaseT::MsgBufBase;
+  using BufBaseT = MsgBufBase;
+  MsgBuf(ssize_t size) : MsgBufBase(size, 0) {}
+  MsgBuf(const char* buf, ssize_t size) : MsgBufBase(buf, size, 0) {}
+  MsgBuf(const std::string& buf) : MsgBufBase(buf, 0) {}
 };
 }  // namespace MsgHead
--- src_lib_tests/boost_asio/msg_head_test.cpp
+++ src_lib_tests/boost_asio/msg_head_test.cpp
@@ -18,113 +18,107 @@ TEST(msg_buf, 1) {
   auto buf2 = buf1;
   ASSERT_TRUE(str == buf2.Buf());
   ASSERT_EQ(buf2.BufSize(), str.size());
-  ASSERT_NE(buf1.Buf(),buf2.Buf());
+  ASSERT_NE(buf1.Buf(), buf2.Buf());
 
   // move
-  char * buf2_addr = buf2.Buf();
+  char* buf2_addr = buf2.Buf();
   auto buf3 = std::move(buf2);
-  ASSERT_EQ(buf2_addr,buf3.Buf());
+  ASSERT_EQ(buf2_addr, buf3.Buf());
   ASSERT_TRUE(str == buf3.Buf());
   ASSERT_EQ(buf3.BufSize(), str.size());
 
   LOG_DEBUG(std::string(buf3.Buf()))
 }
-struct Padding{
+struct CustomeHead {
   int i1{};
   char c2{};
   double d3{};
   char data[50]{};
-  void ToBuf(char* dest,ssize_t len) const {
-    assert(len>=sizeof (Padding));
-    memcpy(dest, this, sizeof(Padding));
+  void ToBuf(char* dest, ssize_t len) const {
+    assert(len >= sizeof(CustomeHead));
+    memcpy(dest, this, sizeof(CustomeHead));
   }
-  void FromBuf(const char* from,ssize_t len) {
-    assert(len>=sizeof (Padding));
-    memcpy(this, from, sizeof(Padding));
+  void FromBuf(const char* from, ssize_t len) {
+    assert(len >= sizeof(CustomeHead));
+    memcpy(this, from, sizeof(CustomeHead));
   }
 };
-static_assert(std::is_trivially_copyable_v<Padding>);
-TEST(pad_msg_buf,1)
-{
-  using MsgBufT = PadStructMsgBuf<Padding>;
-  Padding pad{.i1=3,.d3=3.14,.data="world"};
-
+static_assert(std::is_trivially_copyable_v<CustomeHead>);
+TEST(pad_msg_buf, 1) {
+  using MsgBufT = CustomStructHeadMsgBuf<CustomeHead>;
+  CustomeHead pad{.i1 = 3, .d3 = 3.14, .data = "world"};
 
   MsgBufT buf(100);
   ASSERT_EQ(buf.BufSize(), 100);
-  
+
   std::string str("hello");
   MsgBufT buf1(str);
-  buf1.SetPad(pad);
+  buf1.SetCustomHead(pad);
   ASSERT_TRUE(str == buf1.Buf());
   ASSERT_EQ(buf1.BufSize(), str.size());
-  Padding read_pad{};
+  CustomeHead read_pad{};
 
-  memset(&read_pad,0,sizeof (Padding));
-  buf1.ReadPad(read_pad);
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  buf1.ReadCustomHead(read_pad);
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   // copy
   auto buf2 = buf1;
   ASSERT_TRUE(str == buf2.Buf());
   ASSERT_EQ(buf2.BufSize(), str.size());
-  ASSERT_NE(buf1.Buf(),buf2.Buf());
-  memset(&read_pad,0,sizeof (Padding));
-  buf2.ReadPad(read_pad);
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
+  ASSERT_NE(buf1.Buf(), buf2.Buf());
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  buf2.ReadCustomHead(read_pad);
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   // move
-  char * buf2_addr = buf2.Buf();
+  char* buf2_addr = buf2.Buf();
   auto buf3 = std::move(buf2);
-  ASSERT_EQ(buf2_addr,buf3.Buf());
+  ASSERT_EQ(buf2_addr, buf3.Buf());
   ASSERT_TRUE(str == buf3.Buf());
   ASSERT_EQ(buf3.BufSize(), str.size());
-  memset(&read_pad,0,sizeof (Padding));
-  buf3.ReadPad(read_pad);
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
-
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  buf3.ReadCustomHead(read_pad);
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   LOG_DEBUG(std::string(buf3.Buf()))
 }
 
-TEST(pad_buf,1)
-{
-  using MsgBufT = PadMsgBuf<sizeof (Padding)>;
-  Padding pad{.i1=3,.d3=3.14,.data="world"};
+TEST(pad_buf, 1) {
+  using MsgBufT = CustomHeadMsgBuf;
+  CustomeHead pad{.i1 = 3, .d3 = 3.14, .data = "world"};
 
-
-  MsgBufT buf(100);
+  MsgBufT buf(100, sizeof(CustomeHead));
   ASSERT_EQ(buf.BufSize(), 100);
 
   std::string str("hello");
-  MsgBufT buf1(str);
-  pad.ToBuf(buf1.PadBuf(),buf1.PadBufSize());
+  MsgBufT buf1(str, sizeof(CustomeHead));
+  pad.ToBuf(buf1.CustomHead(), buf1.CustomHeadSize());
   ASSERT_TRUE(str == buf1.Buf());
   ASSERT_EQ(buf1.BufSize(), str.size());
-  Padding read_pad{};
-  memset(&read_pad,0,sizeof (Padding));
-  read_pad.FromBuf(buf1.PadBuf(),buf1.PadBufSize());
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
+  CustomeHead read_pad{};
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  read_pad.FromBuf(buf1.CustomHead(), buf1.CustomHeadSize());
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   // copy
   auto buf2 = buf1;
   ASSERT_TRUE(str == buf2.Buf());
   ASSERT_EQ(buf2.BufSize(), str.size());
-  ASSERT_NE(buf1.Buf(),buf2.Buf());
-  memset(&read_pad,0,sizeof (Padding));
-  read_pad.FromBuf(buf2.PadBuf(),buf2.PadBufSize());
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
+  ASSERT_NE(buf1.Buf(), buf2.Buf());
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  read_pad.FromBuf(buf2.CustomHead(), buf2.CustomHeadSize());
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   // move
-  char * buf2_addr = buf2.Buf();
+  char* buf2_addr = buf2.Buf();
   auto buf3 = std::move(buf2);
-  ASSERT_EQ(buf2_addr,buf3.Buf());
+  ASSERT_EQ(buf2_addr, buf3.Buf());
   ASSERT_TRUE(str == buf3.Buf());
   ASSERT_EQ(buf3.BufSize(), str.size());
-  memset(&read_pad,0,sizeof (Padding));
-  read_pad.FromBuf(buf3.PadBuf(),buf3.PadBufSize());
-  ASSERT_EQ(memcmp(&pad,&read_pad,sizeof (Padding)),0);
-
+  memset(&read_pad, 0, sizeof(CustomeHead));
+  read_pad.FromBuf(buf3.CustomHead(), buf3.CustomHeadSize());
+  ASSERT_EQ(memcmp(&pad, &read_pad, sizeof(CustomeHead)), 0);
 
   LOG_DEBUG(std::string(buf3.Buf()))
 }
