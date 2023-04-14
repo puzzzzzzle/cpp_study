d13550917627e097d99716439fdee43e92a041f3
puzzzzzzle <2359173906@qq.com>
2023-04-14 14:49:07 +0800

    add simple co

--- src_cxx_newest/co_samples/my_co_test/generator.h
+++ src_cxx_newest/co_samples/my_co_test/generator.h
@@ -27,6 +27,14 @@ struct promise_type_base {
   void unhandled_exception() { std::exit(1); }
 };
 template <typename T>
+struct suspend_always_send_value : std::suspend_always {
+  constexpr auto await_resume() const { return m_p->send_data_; }
+
+  constexpr suspend_always_send_value(T* p) : m_p(p) {}
+  T* m_p;
+};
+
+template <typename T>
 struct generator_base {
   T& underlying() { return static_cast<T&>(*this); }
   T const& underlying() const { return static_cast<T const&>(*this); }
@@ -78,13 +86,6 @@ struct generator_base {
   }
 };
 
-template <typename T>
-struct suspend_always_send_value : std::suspend_always {
-  constexpr auto await_resume() const { return m_p->send_data_; }
-
-  constexpr suspend_always_send_value(T* p) : m_p(p) {}
-  T* m_p;
-};
 // 可以 send 值 给协程, 协程也可以yield/return值给调用放
 template <typename YIELD_TYPE, typename RET_TYPE, typename SEND_TYPE>
 struct generator
@@ -123,39 +124,6 @@ struct generator
     SEND_TYPE send_data_;
   };
 };
-// 协程可以yield/return值给调用方, 调用方不可以传值给协程
-template <typename YIELD_TYPE, typename RET_TYPE>
-struct generator_yield_ret
-    : public generator_base<generator_yield_ret<YIELD_TYPE, RET_TYPE>> {
-  class promise_type;
-  using handle_type = std::coroutine_handle<promise_type>;
-  using generator_base<generator_yield_ret>::generator_base;
-
-  generator_yield_ret(handle_type h) : coro_handle_(h) {}
-
-  //! 协程句柄
-  handle_type coro_handle_;
-
-  bool send() { return generator_base<generator_yield_ret>::send_base(); }
-
-  RET_TYPE get_ret() { return coro_handle_.promise().return_data_; }
-  YIELD_TYPE get_yield() { return coro_handle_.promise().yield_data_; }
-
-  struct promise_type : public promise_type_base<promise_type> {
-    template <class T>
-    void return_value(RET_TYPE v) { return_data_ = v; }
-    auto yield_value() {
-      yield_data_ = v;
-      return std::suspend_always();
-    }
-    auto get_return_object() {
-      auto t = handle_type::from_promise(*this);
-      return generator_yield_ret{t};
-    }
-    YIELD_TYPE yield_data_;
-    RET_TYPE return_data_;
-  };
-};
 // 只是通知执行, 不进行值交换
 struct generator_void : public generator_base<generator_void> {
   class promise_type;
@@ -173,14 +141,14 @@ struct generator_void : public generator_base<generator_void> {
     template <class T>
     friend class suspend_always_send_value;
     void return_void() {}
-    auto yield_value() { return std::suspend_always{}; }
+    auto yield_value(int) { return std::suspend_always{}; }
     auto get_return_object() {
       auto t = handle_type::from_promise(*this);
       return generator_void{t};
     }
   };
 };
-// 协程可以yield/return值给调用方, 调用方不可以传值给协程
+// 协程可以yield值给调用方
 template <typename YIELD_TYPE>
 struct generator_yield : public generator_base<generator_yield<YIELD_TYPE>> {
   class promise_type;
@@ -197,12 +165,10 @@ struct generator_yield : public generator_base<generator_yield<YIELD_TYPE>> {
   YIELD_TYPE get_yield() { return coro_handle_.promise().yield_data_; }
 
   struct promise_type : public promise_type_base<promise_type> {
-    template <class T>
-    friend class suspend_always_send_value;
     void return_void() {}
     auto yield_value(YIELD_TYPE v) {
       yield_data_ = v;
-      return suspend_always_send_value(this);
+      return std::suspend_always();
     }
     auto get_return_object() {
       auto t = handle_type::from_promise(*this);
@@ -211,7 +177,7 @@ struct generator_yield : public generator_base<generator_yield<YIELD_TYPE>> {
     YIELD_TYPE yield_data_;
   };
 };
-// 协程可以yield/return值给调用方, 调用方不可以传值给协程
+// 协程可以return值给调用方, 调用方不可以传值给协程
 template <typename RET_TYPE>
 struct generator_ret : public generator_base<generator_ret<RET_TYPE>> {
   class promise_type;
@@ -231,12 +197,41 @@ struct generator_ret : public generator_base<generator_ret<RET_TYPE>> {
     template <class T>
     friend class suspend_always_send_value;
     void return_value(RET_TYPE v) { return_data_ = v; }
-    auto yield_value() { return suspend_always_send_value(this); }
+    auto yield_value(int) { return std::suspend_always(); }
     auto get_return_object() {
       auto t = handle_type::from_promise(*this);
       return generator_ret{t};
     }
     RET_TYPE return_data_;
+  };
+};
+// 可以 send 值 给协程
+template <typename SEND_TYPE>
+struct generator_send : public generator_base<generator_send<SEND_TYPE>> {
+  class promise_type;
+  using handle_type = std::coroutine_handle<promise_type>;
+  using generator_base<generator_send>::generator_base;
+
+  generator_send(handle_type h) : coro_handle_(h) {}
+
+  //! 协程句柄
+  handle_type coro_handle_;
+
+  bool send(const SEND_TYPE& send) {
+    coro_handle_.promise().send_data_ = send;
+    return generator_base<generator_send>::send_base();
+  }
+
+  struct promise_type : public promise_type_base<promise_type> {
+    template <class T>
+    friend class suspend_always_send_value;
+    void return_void() {}
+    auto yield_value(int) { return suspend_always_send_value(this); }
+    auto get_return_object() {
+      auto t = handle_type::from_promise(*this);
+      return generator_send{t};
+    }
+    SEND_TYPE send_data_;
   };
 };
 }  // namespace coro
--- src_cxx_newest/co_samples/my_co_test/generator_test.cpp
+++ src_cxx_newest/co_samples/my_co_test/generator_test.cpp
@@ -7,24 +7,22 @@
 using namespace coro;
 
 // send test
-generator<int,int, int> consumer() {
+generator_send<int> consumer() {
   while (true) {
-    // co_yield void 就行 没意义 暂未适配void
-    auto n = co_yield -1;
+    // co_yield void 目前的协程机制不支持
+    auto n = co_yield 0;
     LOG_DEBUG(" -- consume : " << n)
   }
-  // co_return void 就行 没意义 暂未适配void
-  co_return -1;
+  co_return;
 }
-generator<int,int, int> producer(generator<int,int, int>& c) {
+generator_void producer(generator_send<int>& c) {
   c.start();
   for (int i = 0; i < 5; ++i) {
     auto data = i + 1000;
     LOG_DEBUG(" -- produce: " << data)
     c.send(data);
   }
-  // co_return void 就行 没意义 暂未适配void
-  co_return -1;
+  co_return;
 }
 void consumer_producer_test() {
   auto c = consumer();
@@ -33,18 +31,42 @@ void consumer_producer_test() {
 }
 TEST(generator, 1) { consumer_producer_test(); }
 //
-//// yield / return test
-//generator<int, void> yield_value(int n) {
-//  for (int i = 0; i < n; ++i) {
-//    co_yield i;
-//  }
-//  co_return 1000;
-//}
-//TEST(generator, yield) {
-//  auto c = yield_value(5);
-//  c.start();
-//  while (!c.done()) {
-//     c.send();
-//    LOG_DEBUG("yield " << c.get())
-//  }
-//}
+// yield  test
+generator_yield<int> yield_value_test(int n) {
+  for (int i = 0; i < n; ++i) {
+    co_yield i;
+  }
+}
+
+TEST(generator, yield) {
+  auto c = yield_value_test(5);
+  c.start();
+  while (!c.done()) {
+    LOG_DEBUG("yield " << c.get_yield())
+    c.send();
+  }
+}
+
+// all test
+// yield , return, send
+generator<std::string, int, double> all_test(int count) {
+  int sum = 0;
+  double send;
+  for (int i = 0; i <= count; ++i) {
+    send = co_yield std::to_string(send) + "_from_coro";
+    sum += (int)send;
+  }
+  co_return sum;
+}
+
+TEST(generator, send) {
+  auto c = all_test(5);
+  c.start();
+  int i = 0;
+  while (!c.done()) {
+    c.send(i+1000.001);
+    ++i;
+    LOG_DEBUG("yield " << c.get_yield())
+  }
+  LOG_DEBUG("return " << c.get_ret())
+}
