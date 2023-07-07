a6cf86e9ec0d028ae1ee78b6b90f5aec57d736e6
puzzzzzzle <2359173906@qq.com>
2023-07-07 15:44:06 +0800

    add

--- src_cpp_language/va_arg_test/main.cpp
+++ src_cpp_language/va_arg_test/main.cpp
@@ -1,5 +1,49 @@
 #include "common_includes.h"
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+#include "stdarg.h"
+namespace test_1 {
+void print_va_arg(const char *first, ...) {
+  LOG_DEBUG(" -- start va arg print :")
+
+  const char *v = first;
+  int cnt{};
+
+  va_list arg_ptr;
+  va_start(arg_ptr, first);
+  while (v != nullptr) {
+    LOG_DEBUG("arg at " << cnt << "\t:\t" << v)
+    ++cnt;
+    v = va_arg(arg_ptr, const char *);
+  }
+  va_end(arg_ptr);
+}
+TEST(test_test, 1) {
+  print_va_arg(nullptr);
+  print_va_arg("hello", nullptr);
+  print_va_arg("hello", "world", nullptr);
+}
+}  // namespace test_1
+
+namespace test_2 {
+void print_va_arg(const char *first, ...) {
+  LOG_DEBUG(" -- start va arg print :")
+  const char *v = first;
+  int cnt{};
+
+  va_list arg_ptr;
+  va_start(arg_ptr, first);
+  while (strcmp(v, "") != 0) {
+    LOG_DEBUG("arg at " << cnt << "\t:\t" << v)
+    ++cnt;
+    v = va_arg(arg_ptr, const char *);
+  }
+  va_end(arg_ptr);
+}
+TEST(test_test, 2) {
+  print_va_arg("");
+  print_va_arg("hello", "");
+  print_va_arg("hello", "world", "");
+}
+}  // namespace test_2
 
 int main(int argc, char **argv) {
   int iRet = 0;
