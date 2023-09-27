19171f882502757c894716d001fe3a5f1d3f4fa6
puzzzzzzle <2359173906@qq.com>
2023-09-28 03:54:20 +0800

    update

--- src_lib_clang_study/cpp_header_tools/test_main.py
+++ src_lib_clang_study/cpp_header_tools/test_main.py
@@ -9,28 +9,32 @@ def fix_path(cmd: str, old, new):
     return cmd.replace(old, new)
 
 
-def win_main():
-    win_path = Path("../..").absolute()
-    linux_path = "/tmp/cpp_study"
-    cmake_dir = "cmake-build-debug-docker"
-    with open(win_path / cmake_dir / "compile_commands.json") as f:
-        db = json.load(f)
-    proc_item = None
-    for item in db:
-        name: str = item["file"]
-        if name.endswith("book_actor.cpp"):
-            proc_item = item
-            break
-    assert proc_item is not None
-    cmd = fix_path(proc_item["command"], linux_path, str(win_path))
-    src = fix_path(proc_item["file"], linux_path, str(win_path))
-    out = win_path / "src_lib_clang_study/generated_test/"
-    ch.generated_one(cmd, src, out)
-    # proc_item = fix_compile_db(proc_item,"")
+# def win_main():
+#     win_path = Path("../..").absolute()
+#     linux_path = "/tmp/cpp_study"
+#     cmake_dir = "cmake-build-debug-docker"
+#     with open(win_path / cmake_dir / "compile_commands.json") as f:
+#         db = json.load(f)
+#     proc_item = None
+#     for item in db:
+#         name: str = item["file"]
+#         if name.endswith("book_actor.cpp"):
+#             proc_item = item
+#             break
+#     assert proc_item is not None
+#     cmd = fix_path(proc_item["command"], linux_path, str(win_path))
+#     src = fix_path(proc_item["file"], linux_path, str(win_path))
+#     out = win_path / "src_lib_clang_study/generated_test/"
+#     ch.generated_one(cmd, src, out)
+#     # proc_item = fix_compile_db(proc_item,"")
+#     import clang.cindex as cl
+#     compile_db:cl.CompilationDatabase = cl.CompilationDatabase.fromDirectory(str(win_path))
+#     pass
 
 
 def linux_main():
-    cmd_db = Path("/tmp/tmp.O6S6c6tjik/cmake-build-debug-docker/")
+    build_path = "/tmp/tmp.P2TjzjZZfu"
+    cmd_db = Path(f"{build_path}/cmake-build-debug-docker/")
     with open(cmd_db / "compile_commands.json") as f:
         db = json.load(f)
     proc_item = None
@@ -42,13 +46,12 @@ def linux_main():
     assert proc_item is not None
     cmd = proc_item["command"]
     src = proc_item["file"]
-    out = Path("/tmp/tmp.O6S6c6tjik/") / "src_lib_clang_study/generated_test/"
+    out = Path(build_path) / "src_lib_clang_study/generated_test/"
     ch.generated_one(cmd, src, out)
     import clang.cindex as cl
-    index = cl.Index.create()
 
     # Step 1: load the compilation database
-    compdb:cl.CompilationDatabase = cl.CompilationDatabase.fromDirectory(str(cmd_db))
+    compdb: cl.CompilationDatabase = cl.CompilationDatabase.fromDirectory(str(cmd_db))
     pass
 
 
