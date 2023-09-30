22796b59ee55a15dda968c60af3dbf5348a127f1
puzzzzzzle <2359173906@qq.com>
2023-10-01 02:52:40 +0800

    update

--- src_lib_clang_study/cpp_header_tools/cpp_header_tools/template/ch_priority/template_main.py
+++ src_lib_clang_study/cpp_header_tools/cpp_header_tools/template/ch_priority/template_main.py
@@ -48,7 +48,7 @@ return this->{{ field_name }};
 
 {% if gen_setter %}
 // setter for {{ field_name }}
-void {{ namespace }}::{{ class_name }}::set_{{ field_alias }}({{ filed_type }} {{ field_alias }}_val)
+void {{ namespace }}::{{ class_name }}::set_{{const field_alias }}({{ filed_type }}& {{ field_alias }}_val)
 {
 this->{{ field_name }} = {{ field_alias }}_val;
 }
