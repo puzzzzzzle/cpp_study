a5ab2a8f0aab045c8d553b9fed331ab44b67e58d
puzzzzzzle <2359173906@qq.com>
2024-01-03 16:33:07 +0800

    change comment

--- src_cpp_language/multi_thread_write_int/main.cpp
+++ src_cpp_language/multi_thread_write_int/main.cpp
@@ -2,61 +2,65 @@
 #include <string>
 #include <thread>
 #include <vector>
+
+int unused = 42;
+
 // 正常, 每个线程有自己的变量
-//thread_local int level = 0;
+// thread_local int level = 0;
 
 // 多线程异常
-//int level = 0;
+ int level = 0;
 
-// 多线程异常, i++ 是#~5个指令 : mem -> cache, cache -> reg, add reg, reg -> cache, cache -> mem, 无法保证原子性
-//volatile int level = 0;
+// 多线程异常, i++ 是#~5个指令 : mem -> cache, cache -> reg, add reg, reg ->
+// cache, cache -> mem, 无法保证原子性
+// volatile int level = 0;
 
 // 正常
-std::atomic_int level = 0;
+//std::atomic_int level = 0;
 
 class Test {
-  std::string m_prefix;
-  std::string str_;
-
   public:
-  Test(std::string str)
-      : m_prefix(std::string(level * 2, '+')), str_(std::move(str)) {
+  Test() {
+    if (level < 0) {
+      throw std::runtime_error("level < 0: (" + std::to_string(level) +")");
+    }
     level++;
   }
   ~Test() { level--; }
-  void DoSomething(int recursiveLevel = 0) {
-    if (rand() % 100 < 50 && recursiveLevel < 5) {
-      DoSomething(++recursiveLevel);
-    }
-
-    if ("test" != str_) {
-      std::cerr << "wrong ....." << std::endl;
-    }
-  }
+  void UpdateLevel() { unused = level; }
 };
-void TestFunc() {
-  Test test("test");
-  test.DoSomething();
+void DoSomething(int recursiveLevel = 0) {
+  Test test{};
+  test.UpdateLevel();
+  if (recursiveLevel < 5) {
+    DoSomething(++recursiveLevel);
+  }
 }
+void TestFunc() { DoSomething(0); }
 void Run(int n) {
+  auto t = std::time(nullptr);
   for (int i = 0; i < n; i++) {
-    if (i % 1000000 == 0) {
+    if (i % 5000000 == 0) {
+      auto curr = std::time(nullptr);
       std::cout << std::this_thread::get_id() << " run at " << i << "/" << n
-                << std::endl;
+                << "\ttime use: " << (curr - t) << std::endl;
+      t = curr;
     }
     try {
       TestFunc();
     } catch (const std::exception &e) {
-      std::cerr << "catch std exception: " << e.what() <<" level "<< level << std::endl;
+      std::cerr << "catch std exception: " << e.what() << " level " << level
+                << std::endl;
     } catch (...) {
       std::cerr << "catch unknown exception: " << std::endl;
     }
   }
+  std::cout << unused << "\t" << level << std::endl;
 }
 int main(int argc, char **argv) {
   std::vector<std::thread> threads;
   for (int i = 0; i < 10; i++) {
-    threads.emplace_back(Run, 10000000);
+    threads.emplace_back(Run, 11000000);
   }
   for (auto &t : threads) {
     t.join();
