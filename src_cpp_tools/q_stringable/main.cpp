d2f7c8d9a68190d101aeb87a61095a76257e3b6f
puzzzzzzle <2359173906@qq.com>
2024-04-25 15:45:46 +0800

    add ToString

--- src_cpp_tools/q_stringable/main.cpp
+++ src_cpp_tools/q_stringable/main.cpp
@@ -1,3 +1,5 @@
+#include <time_gap.hpp>
+
 #include "common_includes.h"
 #include "Stringable.h"
 #include "Jsonable.h"
@@ -106,6 +108,161 @@ TEST(test_test, json_test) {
             R"([{"i_":1,"str_":"ssss1"},{"i_":1,"str_":"ssss1"},{"i_":1,"str_":"ssss1"},{"i_":1,"str_":"ssss1"},{"i_":1,"str_":"ssss1"}])");
 }
 
+class Point : public Stringable<Point> {
+public:
+  int x_{};
+  int y_{};
+  Point() = default;
+
+  Point(int x, int y): x_(x), y_(y) {
+  }
+
+  std::ostream& ToStringImpl(std::ostream& os) const {
+    os << "y:" << x_;
+    os << "y:" << y_;
+    return os;
+  }
+};
+
+/**
+ * 虚函数版, 测试性能
+ */
+class VStringable : public Stringable<VStringable> {
+public:
+  std::ostream& ToStringImpl(std::ostream& os) const {
+    return VToStringImpl(os);
+  }
+
+  virtual ~VStringable() = default;
+  virtual std::ostream& VToStringImpl(std::ostream& os) const =0;
+};
+
+class VPoint : public VStringable {
+public:
+  int x_{};
+  int y_{};
+  VPoint() = default;
+
+  VPoint(int x, int y): x_(x), y_(y) {
+  }
+
+  std::ostream& VToStringImpl(std::ostream& os) const override {
+    os << "y:" << x_;
+    os << "y:" << y_;
+    return os;
+  }
+};
+
+template <class T>
+void MakePoints(int count = 10000000) {
+  std::vector<T> points{};
+  TimeGap gap{};
+
+  // --------
+  for (int i = 0; i < count; ++i) {
+    points.emplace_back(123, 456);
+  }
+  // 提前Cache time, 防止日志输出时造成误差
+  gap.cacheTime();
+  LOG_DEBUG(
+      "make time :  "<<"type:"<<typeid(T).name()<<"\tcount:" << count <<
+      "\ttime(us):"<<gap.gapCacheUs() <<",(ms)"<<gap.gapCacheMs())
+
+  // --------
+  gap.resetStartNow();
+  std::vector<T> points1 = points;
+  // 提前Cache time, 防止日志输出时造成误差
+  gap.cacheTime();
+  LOG_DEBUG(
+      "copy time :  "<<"type:"<<typeid(T).name()<<"\tcount:" << count <<
+      "\ttime(us):"<<gap.gapCacheUs() <<",(ms)"<<gap.gapCacheMs())
+
+  // --------
+  gap.resetStartNow();
+  points.clear();
+  // 提前Cache time, 防止日志输出时造成误差
+  gap.cacheTime();
+  LOG_DEBUG(
+      "clear points free time :  "<<"type:"<<typeid(T).name()<<"\tcount:" <<
+      count <<
+      "\ttime(us):"<<gap.gapCacheUs() <<",(ms)"<<gap.gapCacheMs())
+
+  // --------
+  gap.resetStartNow();
+  points1.clear();
+  // 提前Cache time, 防止日志输出时造成误差
+  gap.cacheTime();
+  LOG_DEBUG(
+      "clear points1 free time :  "<<"type:"<<typeid(T).name()<<"\tcount:" <<
+      count <<
+      "\ttime(us):"<<gap.gapCacheUs() <<",(ms)"<<gap.gapCacheMs())
+}
+
+TEST(speed_test, stringable) {
+  /*
+析构差别很大, 是因为析构时
+travelCopyable 版本, 直接标记内存不再使用就行
+非 travelCopyable 版本, 需要遍历调用析构函数
+   */
+  /*
+debug 版本 构造 拷贝 析构 10000000 次
+非虚函数版, 构造耗时1092ms, 拷贝耗时247ms, 析构耗时 0ms
+虚函数版, 构造耗时1658ms, 拷贝耗时340ms, 析构耗时 221ms
+   */
+  /*
+release O3 版本 构造 拷贝 析构 10000000 次
+非虚函数版, 构造耗时127ms, 拷贝耗时52ms, 析构耗时 0ms
+虚函数版, 构造耗时264ms, 拷贝耗时84ms, 析构耗时 24ms
+   */
+  for (int i = 0; i < 3; ++i) {
+    TimeGap gap{};
+    MakePoints<Point>();
+    LOG_DEBUG(
+        "make copy free*2 total time :" << "\ttime(us):"<<gap.gapUs() <<",(ms)"
+        <<gap.gapMs())
+    printf("\n");
+  }
+  printf("-------------\n\n");
+  for (int i = 0; i < 3; ++i) {
+    TimeGap gap{};
+    MakePoints<VPoint>();
+    LOG_DEBUG(
+        "make copy free*2 total time :" << "\ttime(us):"<<gap.gapUs() <<
+        ",(ms)"<<gap.gapMs())
+    printf("\n");
+  }
+}
+
+/*
+debug 版
+
+[14] [DEBUG] [2024-04-25 07:39:46 103ms:618µs:210ns] [make time :  type:5Point	count:10000000	time(us):1092356,(ms)1092] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:167  void MakePoints(int) [with T = Point]]
+[15] [DEBUG] [2024-04-25 07:39:46 351ms:105µs:286ns] [copy time :  type:5Point	count:10000000	time(us):247336,(ms)247] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:176  void MakePoints(int) [with T = Point]]
+[16] [DEBUG] [2024-04-25 07:39:46 351ms:173µs:594ns] [clear points free time :  type:5Point	count:10000000	time(us):0,(ms)0] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:185  void MakePoints(int) [with T = Point]]
+[17] [DEBUG] [2024-04-25 07:39:46 351ms:266µs:299ns] [clear points1 free time :  type:5Point	count:10000000	time(us):0,(ms)0] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:195  void MakePoints(int) [with T = Point]]
+
+-------------
+
+[19] [DEBUG] [2024-04-25 07:39:48 24ms:144µs:567ns] [make time :  type:6VPoint	count:10000000	time(us):1658179,(ms)1658] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:167  void MakePoints(int) [with T = VPoint]]
+[20] [DEBUG] [2024-04-25 07:39:48 364ms:850µs:419ns] [copy time :  type:6VPoint	count:10000000	time(us):340550,(ms)340] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:176  void MakePoints(int) [with T = VPoint]]
+[21] [DEBUG] [2024-04-25 07:39:48 586ms:485µs:390ns] [clear points free time :  type:6VPoint	count:10000000	time(us):221555,(ms)221] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:185  void MakePoints(int) [with T = VPoint]]
+[22] [DEBUG] [2024-04-25 07:39:48 808ms:390µs:64ns] [clear points1 free time :  type:6VPoint	count:10000000	time(us):221761,(ms)221] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:195  void MakePoints(int) [with T = VPoint]]
+ */
+/*
+release O3 版
+
+[14] [DEBUG] [2024-04-25 07:39:13 772ms:492µs:913ns] [make time :  type:5Point	count:10000000	time(us):127575,(ms)127] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:167  void MakePoints(int) [with T = Point]]
+[15] [DEBUG] [2024-04-25 07:39:13 824ms:818µs:677ns] [copy time :  type:5Point	count:10000000	time(us):52236,(ms)52] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:176  void MakePoints(int) [with T = Point]]
+[16] [DEBUG] [2024-04-25 07:39:13 824ms:962µs:835ns] [clear points free time :  type:5Point	count:10000000	time(us):0,(ms)0] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:185  void MakePoints(int) [with T = Point]]
+[17] [DEBUG] [2024-04-25 07:39:13 824ms:991µs:764ns] [clear points1 free time :  type:5Point	count:10000000	time(us):0,(ms)0] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:195  void MakePoints(int) [with T = Point]]
+
+-------------
+
+[19] [DEBUG] [2024-04-25 07:39:14 103ms:867µs:919ns] [make time :  type:6VPoint	count:10000000	time(us):264105,(ms)264] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:167  void MakePoints(int) [with T = VPoint]]
+[20] [DEBUG] [2024-04-25 07:39:14 188ms:880µs:846ns] [copy time :  type:6VPoint	count:10000000	time(us):84912,(ms)84] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:176  void MakePoints(int) [with T = VPoint]]
+[21] [DEBUG] [2024-04-25 07:39:14 213ms:129µs:978ns] [clear points free time :  type:6VPoint	count:10000000	time(us):24091,(ms)24] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:185  void MakePoints(int) [with T = VPoint]]
+[22] [DEBUG] [2024-04-25 07:39:14 239ms:807µs:433ns] [clear points1 free time :  type:6VPoint	count:10000000	time(us):26547,(ms)26] [/tmp/tmp.QIYvyIJf56/src_cpp_tools/q_stringable/main.cpp:195  void MakePoints(int) [with T = VPoint]]
+ */
 int main(int argc, char** argv) {
   int iRet = 0;
 
