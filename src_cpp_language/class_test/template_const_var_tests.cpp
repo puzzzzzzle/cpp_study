9816e9be552ccb6105c136b95131dece39236240
puzzzzzzle <2359173906@qq.com>
2024-05-07 03:21:14 +0000

    update

--- src_cpp_language/class_test/template_const_var_tests.cpp
+++ src_cpp_language/class_test/template_const_var_tests.cpp
@@ -3,11 +3,31 @@
 //
 #include "common_includes.h"
 namespace TemplateConstVarTest {
+// constexpr 可以在编译期求值, 相当于常数
 template <class T, T v>
 struct intellij_constant {
   static constexpr T value = v;
 };
 intellij_constant<int, 5> FIVE;
 
-TEST(TemplateConstVarTest, 1) { LOG_DEBUG(FIVE.value) }
+// 非 constexpr 要在外部初始化
+template <class T, T v>
+struct intellij_constant_old {
+  static T value;
+};
+template <class T, T v>
+T intellij_constant_old<T, v>::value = v;
+intellij_constant_old<int, 6> SIX;
+
+//template <class T, T v>
+//struct intellij_constant_err {
+//  static T value = v;
+//};
+//intellij_constant_err<int, 8> EIGHT;
+
+
+TEST(TemplateConstVarTest, 1) {
+  LOG_DEBUG(FIVE.value);
+  LOG_DEBUG(SIX.value);
+}
 }  // namespace TemplateConstVarTest
