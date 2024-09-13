3cac75189c159d5c09572159852839a912d58baa
puzzzzzzle <2359173906@qq.com>
2024-09-13 17:24:10 +0800

    update

--- cpp_shell.sh.cpp
+++ cpp_shell.sh.cpp
@@ -6,7 +6,7 @@ SOURCE_NAME=$(basename "$0") ; c++ -O3 -o /tmp/$SOURCE_NAME.out "$0" && /tmp/$SO
 // 这样就可以像shell一样运行c++源码"脚本"了
 // ./cpp_shell.sh.cpp 直接运行...
 
-// 下面这种也等价, 只是稍微少点注释
+// 下面这种也等价, 只是稍微少点注释, 当然, 展开成多行脚本文件也可以
 #if 0
 SOURCE_NAME=$(basename "$0") && c++ -O3 -o /tmp/$SOURCE_NAME.out "$0" && /tmp/$SOURCE_NAME.out ; exit;
 #endif
