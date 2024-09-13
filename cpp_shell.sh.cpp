4668021efd13c24c1fb4841447ad4724dcbf609b
puzzzzzzle <2359173906@qq.com>
2024-09-13 17:22:19 +0800

    update

---
+++ cpp_shell.sh.cpp
@@ -0,0 +1,21 @@
+#if 0/* run cpp source file from shell directly, eg: ./cpp_shell.sh.cpp
+SOURCE_NAME=$(basename "$0") ; c++ -O3 -o /tmp/$SOURCE_NAME.out "$0" && /tmp/$SOURCE_NAME.out; exit $?; */
+#endif
+
+// 上面的部分, 会被shell解析, 然后自动编制并运行
+// 这样就可以像shell一样运行c++源码"脚本"了
+// ./cpp_shell.sh.cpp 直接运行...
+
+// 下面这种也等价, 只是稍微少点注释
+#if 0
+SOURCE_NAME=$(basename "$0") && c++ -O3 -o /tmp/$SOURCE_NAME.out "$0" && /tmp/$SOURCE_NAME.out ; exit;
+#endif
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "hello world form c++ 'script'." << std::endl;
+    exit(22);
+    return 22;
+}
