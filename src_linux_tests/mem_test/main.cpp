e6e92aa7a7e425b2909fdc73a3f47485b2ecbf79
puzzzzzzle <2359173906@qq,com>
2024-10-12 10:17:04 +0800

    update

--- src_linux_tests/mem_test/main.cpp
+++ src_linux_tests/mem_test/main.cpp
@@ -1,5 +1,13 @@
 #include "common_includes.h"
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+int test_mem(size_t size) {
+  LOG_DEBUG("will malloc size " << size)
+  char *mem = static_cast<char *>(malloc(size));
+  LOG_DEBUG("will set mem " << size)
+    memset(mem,1,size);
+//  mem[size - 1] = 0;
+  free(mem);
+}
+TEST(test_test, 1) { test_mem(1024L * 1024 * 1024 * 10); }
 
 int main(int argc, char **argv) {
   int iRet = 0;
