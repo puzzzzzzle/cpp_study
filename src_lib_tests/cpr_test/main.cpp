098c55c7c9ca0e2b90f5a45688bcb5dcf8a05324
puzzzzzzle <2359173906@qq.com>
2024-10-29 19:56:51 +0800

    update

--- src_lib_tests/cpr_test/main.cpp
+++ src_lib_tests/cpr_test/main.cpp
@@ -1,6 +1,18 @@
-#include "common_includes.h"
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+#include <cpr/cpr.h>
 
+#include "common_includes.h"
+
+const char *TEST_URL = "http://www.baidu.com/";
+TEST(cpr, simple) {
+  auto rsp = cpr::Get(cpr::Url(TEST_URL));
+  EXPECT_EQ(rsp.status_code, cpr::status::HTTP_OK);
+  LOG_DEBUG(rsp.text);
+}
+TEST(cpr, with_header) {
+  auto rsp = cpr::Get(cpr::Url(TEST_URL),cpr::Header{{"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"},{"Content-Type","text/plain"}});
+  EXPECT_EQ(rsp.status_code, cpr::status::HTTP_OK);
+  LOG_DEBUG(rsp.text);
+}
 int main(int argc, char **argv) {
   int iRet = 0;
 
