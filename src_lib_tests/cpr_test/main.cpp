574871ca070aaf059bdea29e8d7c87507f49aa96
puzzzzzzle <2359173906@qq.com>
2024-10-29 20:08:56 +0800

    update

--- src_lib_tests/cpr_test/main.cpp
+++ src_lib_tests/cpr_test/main.cpp
@@ -9,7 +9,15 @@ TEST(cpr, simple) {
   LOG_DEBUG(rsp.text);
 }
 TEST(cpr, with_header) {
+//  cpr::Authentication auth{"name","passwd",cpr::AuthMode::BASIC};
   auto rsp = cpr::Get(cpr::Url(TEST_URL),cpr::Header{{"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"},{"Content-Type","text/plain"}});
+  EXPECT_EQ(rsp.status_code, cpr::status::HTTP_OK);
+  LOG_DEBUG(rsp.text);
+}
+TEST(cpr, async) {
+  auto rsp_fu = cpr::GetAsync(cpr::Url(TEST_URL),cpr::Header{{"User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"},{"Content-Type","text/plain"}});
+  LOG_DEBUG("get req send, wait rsp")
+  auto rsp = rsp_fu.get();
   EXPECT_EQ(rsp.status_code, cpr::status::HTTP_OK);
   LOG_DEBUG(rsp.text);
 }
