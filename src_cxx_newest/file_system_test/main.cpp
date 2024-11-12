ef4ad493764d9baad4c9e4a558686e93206c4558
puzzzzzzle <2359173906@qq.com>
2024-11-12 20:32:35 +0800

    update

--- src_cxx_newest/file_system_test/main.cpp
+++ src_cxx_newest/file_system_test/main.cpp
@@ -1,5 +1,29 @@
+#include <filesystem>
+
 #include "common_includes.h"
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+namespace fs = std::filesystem;
+
+TEST(path, 1) {
+  fs::path p("./");
+  LOG_DEBUG(p)
+  p = fs::absolute(p);
+  LOG_DEBUG(p)
+
+  ASSERT_TRUE(exists(p));
+  ASSERT_TRUE(is_directory(p));
+  for (const auto& item : fs::directory_iterator(p))
+  {
+    LOG_DEBUG(item.path()<<"\t"<<item.path().filename())
+  }
+  auto dataPath = p/"data";
+  if(fs::exists(dataPath) && fs::is_directory(dataPath))
+  {
+    LOG_DEBUG("data exists, copy one")
+
+    fs::remove_all(p/"data_copy");
+    fs::copy(dataPath,p/"data_copy");
+  }
+}
 
 int main(int argc, char **argv) {
   int iRet = 0;
