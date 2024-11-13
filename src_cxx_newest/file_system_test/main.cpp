b4b026b0f465815b592190e217062ed1b534596c
puzzzzzzle <2359173906@qq.com>
2024-11-13 15:26:57 +0800

    update

--- src_cxx_newest/file_system_test/main.cpp
+++ src_cxx_newest/file_system_test/main.cpp
@@ -11,21 +11,30 @@ TEST(path, 1) {
 
   ASSERT_TRUE(exists(p));
   ASSERT_TRUE(is_directory(p));
-  for (const auto& item : fs::directory_iterator(p))
-  {
-    LOG_DEBUG(item.path()<<"\t"<<item.path().filename())
+  for (const auto& item : fs::directory_iterator(p)) {
+    LOG_DEBUG(item.path() << "\t" << item.path().filename())
   }
-  auto dataPath = p/"data";
-  if(fs::exists(dataPath) && fs::is_directory(dataPath))
-  {
+  auto dataPath = p / "data";
+  if (fs::exists(dataPath) && fs::is_directory(dataPath)) {
     LOG_DEBUG("data exists, copy one")
 
-    fs::remove_all(p/"data_copy");
-    fs::copy(dataPath,p/"data_copy");
+    fs::remove_all(p / "data_copy");
+    // 直接拷贝, 会把软连接变为普通文件
+    //    fs::copy(dataPath,p/"data_copy");
+    //
+    fs::copy(dataPath, p / "data_copy",
+             fs::copy_options::copy_symlinks | fs::copy_options::recursive);
   }
+  bool throwErr = false;
+  try {
+    fs::copy("not_exists", p / "111");
+  } catch (const fs::filesystem_error& r) {
+    throwErr = true;
+  }
+  ASSERT_TRUE(throwErr);
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
   int iRet = 0;
 
   testing::InitGoogleTest(&argc, argv);
