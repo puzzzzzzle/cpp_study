c1e49c870045d4c9e7c0c0ce2a7e7bc9c87c9666
puzzzzzzle <2359173906@qq.com>
2025-01-30 19:14:48 +0800

    update

--- src_lib_tests/proxy_test/main.cpp
+++ src_lib_tests/proxy_test/main.cpp
@@ -1,7 +1,20 @@
+#include <memory>
+
 #include "common_includes.h"
 #include "proxy/proxy.h"
+struct Formattable : pro::facade_builder ::support_format ::build {};
 
-TEST(test_test, 1) { EXPECT_EQ(1, 1); }
+TEST(format, 1) {
+  std::string str = "Hello World";
+  pro::proxy<Formattable> p1 = &str;
+  FLOG_DEBUG("p1 = {}", *p1);  // Prints "p1 = Hello World"
+
+  pro::proxy<Formattable> p2 = std::make_unique<int>(123);
+  FLOG_DEBUG("p2 = {}", *p2);  // Prints "p2 = 123"
+
+  pro::proxy<Formattable> p3 = pro::make_proxy<Formattable>(3.14159);
+  FLOG_DEBUG("p3 = {:.2f}", *p3);  // Prints "p3 = 3.14"
+}
 
 int main(int argc, char **argv) {
   int iRet = 0;
