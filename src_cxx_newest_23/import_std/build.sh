a71b269acb1f61f6eb5073505cd9d7bca6807d19
puzzzzzzle <2359173906@qq.com>
2025-05-06 19:50:29 +0800

    update

--- src_cxx_newest_23/import_std/build.sh
+++ src_cxx_newest_23/import_std/build.sh
@@ -1,2 +1,3 @@
-g++ -std=c++23 -fmodules-ts -xc++-system-header vector iostream
+g++ -std=c++23 -fmodules-ts -xc++-system-header iostream
+g++ -std=c++23 -fmodules-ts -xc++-system-header vector
 g++ -std=gnu++23 -fmodules main.cpp -o main
