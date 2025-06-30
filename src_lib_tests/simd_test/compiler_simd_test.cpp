0d411651b238d51d8794f96e089c448e51473c13
puzzzzzzle <2359173906@qq.com>
2025-06-30 12:09:32 +0800

    add simd test

--- src_lib_tests/simd_test/compiler_simd_test.cpp
+++ src_lib_tests/simd_test/compiler_simd_test.cpp
@@ -10,14 +10,19 @@
 #include "get_simd_support.h"
 
 // 数据规模
-const int n = 1024 * 1024;
-float src1[n], src2[n], out[n];
-static void init_data() {
+static int init_data(float src1[], float src2[], size_t n) {
   for (int i = 0; i < n; i++) {
-    src1[i] = 1.0f;
-    src2[i] = 2.0f;
+    src1[i] = rand() / float(RAND_MAX);
+    src2[i] = rand() / float(RAND_MAX);
   }
+  return 0;
 }
+// 使用堆变量, 防止编译器过渡优化
+const int n = 1024 * 1024;
+float* src1 = new float[n];
+float* src2 = new float[n];
+float* out = new float[n];
+int init = init_data(src1, src2, n);
 
 #define TEST_TARGET_ADD_FUNC(func_name, simd_type)                            \
   static void BM_##func_name(benchmark::State& state) {                       \
@@ -25,17 +30,22 @@ static void init_data() {
       std::string err_msg = std::string("SIMD not supported :") + #simd_type; \
       state.SkipWithError(err_msg.c_str());                                   \
     }                                                                         \
-    init_data();                                                              \
     for (auto _ : state) {                                                    \
       func_name(src1, src2, out, n);                                          \
     }                                                                         \
   }                                                                           \
   BENCHMARK(BM_##func_name)
 // 标量实现
-void add_scalar(const float* a, const float* b, float* out, size_t n) {
+void add_scalar_restrict(const float* __restrict__ a, const float* __restrict__ b,
+                float* __restrict__ out, size_t n) {
   for (size_t i = 0; i < n; ++i) out[i] = a[i] + b[i];
 }
-TEST_TARGET_ADD_FUNC(add_scalar, SIMD_SUPPORT_NONE);
+TEST_TARGET_ADD_FUNC(add_scalar_restrict, SIMD_SUPPORT_NONE);
+
+void add_scalar_normal(const float* a, const float* b, float* out, size_t n) {
+  for (size_t i = 0; i < n; ++i) out[i] = a[i] + b[i];
+}
+TEST_TARGET_ADD_FUNC(add_scalar_normal, SIMD_SUPPORT_NONE);
 
 // SSE2实现（4个float一组）
 void add_sse2(const float* a, const float* b, float* out, size_t n) {
@@ -64,3 +74,21 @@ void add_avx2(const float* a, const float* b, float* out, size_t n) {
   for (; i < n; ++i) out[i] = a[i] + b[i];
 }
 TEST_TARGET_ADD_FUNC(add_avx2, SIMD_SUPPORT_AVX2);
+
+// 关闭自动向量化优化, 对比下效果
+// -o3 -march=native -fno-tree-vectorize
+//--------------------------------------------------------
+// Benchmark              Time             CPU   Iterations
+//--------------------------------------------------------
+// BM_add_scalar     501072 ns       501066 ns         1000
+// BM_add_sse2       229486 ns       229483 ns         3011
+// BM_add_avx2       227688 ns       227685 ns         3097
+
+// o3 的自动向量化优化还行
+// -o3 -march=native
+//--------------------------------------------------------
+// Benchmark              Time             CPU   Iterations
+//--------------------------------------------------------
+// BM_add_scalar     240029 ns       240016 ns         2472
+// BM_add_sse2       227653 ns       227643 ns         3076
+// BM_add_avx2       229425 ns       229406 ns         3085
