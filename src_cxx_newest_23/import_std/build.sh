07caadcb3f9a5e5cc135c7dd5e87e96639c52888
puzzzzzzle <2359173906@qq.com>
2026-04-12 19:01:12 +0800

    update

--- src_cxx_newest_23/import_std/build.sh
+++
@@ -1,3 +0,0 @@
-g++ -std=c++23 -fmodules-ts -xc++-system-header iostream
-g++ -std=c++23 -fmodules-ts -xc++-system-header vector
-g++ -std=gnu++23 -fmodules main.cpp -o main
--- src_cxx_newest_23/import_std/main.cpp
+++
@@ -1,5 +0,0 @@
-import <iostream>;
-
-int main(int argc, char **argv) {
-  std::cout << "Hello, World!" << std::endl;
-}
